03af6cf29074f66463211e7dc6c3c93b29bf60ff
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2025-08-03 02:22:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors =require('cors');
 const { Pool } = require('pg');
-
-
+const helmet = require('helmet');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
